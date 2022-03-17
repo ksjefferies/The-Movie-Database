b3da3b9846fe2a03e38f21b887ad6e7296337e0e
Kelly Jefferies <k.jefferies@gmail.com>
2022-03-17 13:18:05 -0500

    added additional api's for trailers and genres

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 //jQuery variable elements
 var searchBtn = $("#searchBtn");
-var trailers  = $('#trailers');
+var trailers = $('#trailers');
 
 //global variables
 const api_key = '1fc2de251859dcddc136157f2a89acbe';
@@ -10,28 +10,36 @@ var movieName = '';
 searchBtn.on('click', async function (event) {
     event.preventDefault();
 
+
+    let genre = await genresLookup()
+    console.log(genre)
     movieName = $('#searchInput').val();
-   let  movieResult = await movieLookup(movieName);
+    let movieResult = await movieLookup(movieName);
 
     console.log(movieResult)
     let castResult = await creditLookup(movieResult.results[0].id)
     console.log(castResult)
     let videoResult = await videoLookup(movieResult.results[0].id)
-    console.log(videoResult)
+    let videoTrailer = videoResult.results.filter(function (item) {
+        return item.type == "Trailer"
+    })
+    console.log(videoTrailer[videoTrailer.length - 1].key)
 
 
 
-    trailers.attr('src', "https://www.youtube.com/embed/" + videoResult.results[0].key)
+    trailers.attr('src', "https://www.youtube.com/embed/" + videoTrailer[videoTrailer.length - 1].key)
 })
 
 // movieLookup(movieName)
 // movie ID, original title, overview, backdrop img, poster img, release date, voter average(rating)
+
 async function movieLookup(movieName) {
     let apiSite = 'https://api.themoviedb.org/3/search/movie?';
     let requestParam = {
         api_key: api_key,
         query: movieName,
     };
+
     let queryString = jQuery.param(requestParam);
     let requestUrl = apiSite + queryString;
 
@@ -44,8 +52,9 @@ async function movieLookup(movieName) {
         })
 }
 
+// creditLookup(movieResult.results[0].id) - Cast members
 async function creditLookup(movie_id) {
-    let apiSite = 'https://api.themoviedb.org/3/movie/' + movie_id +'/credits?';
+    let apiSite = 'https://api.themoviedb.org/3/movie/' + movie_id + '/credits?';
     let requestParam = {
         api_key: api_key,
     };
@@ -61,8 +70,9 @@ async function creditLookup(movie_id) {
         })
 }
 
+// videoLookup(movieResult.results[0].id) - Trailers
 async function videoLookup(movie_id) {
-    let apiSite = 'https://api.themoviedb.org/3/movie/' + movie_id +'/videos?';
+    let apiSite = 'https://api.themoviedb.org/3/movie/' + movie_id + '/videos?';
     let requestParam = {
         api_key: api_key,
     };
@@ -78,11 +88,30 @@ async function videoLookup(movie_id) {
         })
 }
 
+async function genresLookup() {
+    let apiGenres = 'https://api.themoviedb.org/3/genre/movie/list?';
+    let requestParam = {
+        api_key: api_key,
+    }
+
+    let queryString = jQuery.param(requestParam);
+    let requestUrl = apiGenres + queryString;
+
+    return fetch(requestUrl)
+        .then(function (response) {
+            return response.text()
+        })
+        .then(function (text) {
+            return JSON.parse(text)
+        })
+}
 
 
 
 
-    
+
+
+
     //fetch OMDB
     // parse into usable array of objects
 
