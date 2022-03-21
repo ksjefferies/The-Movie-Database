38ec43e111af172263ceb1ddbc64fae8620ab078
Kelly Jefferies <k.jefferies@gmail.com>
2022-03-21 00:48:55 -0500

    Corrected all HTML, CSS files.

--- assets/js/script.js
+++ assets/js/script.js
@@ -90,35 +90,6 @@ function displaySearchResults(movieResult) {
   }
 }
 
-// function showSlide(slideIndex) {
-//   const slides = document.getElementsByClassName('carouselImgs');
-//   if (slideIndex > slides.length) { currentSlide = 1 }
-//   if (slideIndex < 1) { currentSlide = slides.length }
-//   for (var i = 0; i < slides.length; i++) {
-//     slides[i].style.display = 'none'
-//   }
-//   slides[currentSlide - 1].style.display = 'flex'
-// }
-
-// Search history slide show (aside index.html)
-// function nextSlide() {
-//   showSlide(currentSlide += 1);
-// }
-
-// function previousSlide() {
-//   showSlide(currentSlide -= 1);
-// }
-
-// window.onload = function () {
-//   showSlide(currentSlide);
-//   document.getElementById('prev').addEventListener('click', function () {
-//     previousSlide();
-//   })
-//   document.getElementById('next').addEventListener('click', function () {
-//     nextSlide();
-//   })
-// }
-
 // *** Console.log statements for testing ***
 // console.log(genre) // (genresLookup)
 // console.log(movieResult) // (movieLookup)
